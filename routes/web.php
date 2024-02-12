00dd33d64bf4347bb9ea63d9fe5e27ad500bc7b2
Rick-Janssen <rick.janssen3107@gmail.com>
2024-02-12 13:56:16 +0100

    login/auth

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BierController;
 use App\Http\Controllers\CommentController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,7 +17,23 @@ use App\Http\Controllers\CommentController;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
 Route::get('/', [BierController::class, 'index'])->name('bier.index');
 Route::get('/bier/{bier}', [BierController::class, 'show'])->name('bier.show');
 
 Route::post('/comments/{bier}', [CommentController::class, 'store'])->name('comments.store');
+
+require __DIR__.'/auth.php';
